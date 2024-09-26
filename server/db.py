7bdb90628ebc4374605973e7ee1fc18a2b41c34c
Punit Arani <punitsai36@gmail.com>
2024-09-26 13:15:00 -0700

    Set engine echo=False

--- server/db.py
+++ server/db.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/postgres")
 
 # Create engine, sessionmaker and base for sqlalchemy
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
